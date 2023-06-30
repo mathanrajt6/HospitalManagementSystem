6697d27672b5f9345634cf50e4df6e2487bb0821
Mathanraj T <mathanraj.kanini@gmail.com>
2023-06-30 18:39:37 +0530

    KANINI HMS - Added CustomException for UserAPI

---
+++ Backend/HMSAPI/HMSUserAPI/Exceptions/CustomException.cs
@@ -0,0 +1,22 @@
+﻿namespace HMSUserAPI.Exceptions
+{
+    public class CustomException
+    {
+    }
+
+    public class ContextException : Exception
+    {
+        public ContextException() : base("Context not found") { }
+        public ContextException(string message) : base(message) { }
+        
+    
+    }
+
+    public class UserException : Exception
+    {
+        public UserException() : base("User Exception raised") { }
+        public UserException(string message) : base(message) { }
+
+
+    }
+}
