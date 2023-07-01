d2b13483833aed32c48e0db0383b775487aed35a
Mathanraj T <mathanraj.kanini@gmail.com>
2023-07-02 03:46:20 +0530

    KANINI HMS - Setup React Project

---
+++ Frontend/kanini-hms/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home/Home';
+import Doctor from './Components/Main/Doctor/Doctor';
+import Doctors from './Components/Main/Doctors/Doctors';
+import Patients from './Components/Main/Patients/Patients';
+import Login from './Components/Home/Login/Login';
+
+function App() {
+  return (
+    <div className="App">
+        <Login/>
+
+
+    </div>
+  );
+}
+
+export default App;
