05f1af94205306d990a2b7b8387b09a3a614567f
Mathanraj T <mathanraj.kanini@gmail.com>
2023-07-01 16:02:44 +0530

    KANINI HMS - Added Validation Filter

---
+++ Backend/HMSAPI/HMSUserAPI/Utility/ValidateModelFilter.cs
@@ -0,0 +1,30 @@
+﻿
+using HMSUserAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HMSUserAPI.Utility
+{
+    public class ValidateModelFilter : Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+           
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var param = context.ActionArguments.SingleOrDefault(p => p.Value is IEntity);
+            if (param.Value == null)
+            {
+                context.Result = new BadRequestObjectResult("Object is null or object field datatype doesn't match");
+                return;
+            }
+
+            if(!context.ModelState.IsValid)
+            {
+                context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+            }
+        }
+    }
+}
