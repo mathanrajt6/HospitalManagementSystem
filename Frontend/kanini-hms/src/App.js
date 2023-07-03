fe4a8b2a613cc74182c1edfdada39a61cfe278c7
Mathanraj T <mathanraj.kanini@gmail.com>
2023-07-03 08:55:10 +0530

    KANINI HMS - Added Asset

--- Frontend/kanini-hms/src/App.js
+++ Frontend/kanini-hms/src/App.js
@@ -5,12 +5,39 @@ import Doctor from './Components/Main/Doctor/Doctor';
 import Doctors from './Components/Main/Doctors/Doctors';
 import Patients from './Components/Main/Patients/Patients';
 import Login from './Components/Home/Login/Login';
+import PatientRegister from './Components/Home/PatientRegister/PatientRegister';
+import DoctorRegister from './Components/Home/DoctorRegister/DoctorRegister';
+import { useState } from 'react';
+import Main from './Components/Main/main';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginProtected from './Components/Protected/LoginProtected';
+import Pending from './Components/Main/Pending/Pendind';
+import Rejected from './Components/Main/Rejected/Rejected';
+import UpdatePassword from './Components/Main/UpdatePassword/UpdatePassword';
 
 function App() {
+
+ 
   return (
     <div className="App">
-        <Login/>
-
+        <BrowserRouter>
+          <Routes>
+            <Route path='/*' element={<Home/>} />
+            {/* <Route path='/home' element={<Home/>} /> */}
+            <Route path='/home/*' element={<Home/>} />
+            {/* <Route path='/main/*' element={
+                        <LoginProtected role={sessionStorage.getItem('role')} > <Doctors/> </LoginProtected>
+              }/> */}
+            {/* <Route path='/main' element={<Main/>}/> */}
+       
+            <Route path='/main/*' element={
+                        <LoginProtected role={sessionStorage.getItem('role')} > <Main/> </LoginProtected>
+            }/>
+                   <Route path='/pending' element={<Pending/>}/>
+                   <Route path='/rejected' element={<Rejected/>}/>
+                   
+          </Routes>
+        </BrowserRouter>
 
     </div>
   );
