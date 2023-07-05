94764b2b800abae89159db877d097a879f10f1b3
Mathanraj T <mathanraj.kanini@gmail.com>
2023-07-05 09:36:06 +0530

    KANINI HMS - Updated Protected for profile

---
+++ Frontend/kanini-hms/src/Components/Protected/ProfileProtected.js
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+function ProfileProtected({role,children})
+{
+    
+    if(role != null && (role === 'admin' || role === 'patient' || role==='doctor'))
+        return children;
+    return <Navigate to="/"/>;
+}
+
+export default ProfileProtected;
