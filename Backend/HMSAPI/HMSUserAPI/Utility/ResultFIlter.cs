c8465212a6b359044cc98a6a27180882d20163fa
Mathanraj T <mathanraj.kanini@gmail.com>
2023-07-05 09:32:31 +0530

    KANINI HMS - Added Result Filter

---
+++ Backend/HMSAPI/HMSUserAPI/Utility/ResultFIlter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HMSUserAPI.Utility
+{
+    public class ResultFIlter : Attribute, IResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            var headerName = "APIName";
+            var headerValue = new string[] { "HMSUserAPI" };
+            context.HttpContext.Response.Headers.Add(headerName, headerValue);
+        }
+    }
+}
